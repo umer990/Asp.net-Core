b3cd1ec9dd4cef2829b96e398cea79df8ce7683f
Umer Malik Khan <umerkhan@outlook.com>
2018-06-13 16:30:12 +0200

    Add files via upload
    All code from day 1

---
+++ dotNet/myApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i="Malik";
+            var j ="Khan";
+            Console.WriteLine("Hello World1!");
+            Console.ReadLine();
+        }
+    }
+}
